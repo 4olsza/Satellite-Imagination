d3f91c860bd895a96bc52edfbe0a34726bed9b6a
4olsza <olszamaciek@gmail.com>
2026-05-27 00:35:21 +0200

    Fix os.path.join bug and ignore macOS hidden files in MapDataset

--- dataset.py
+++ dataset.py
@@ -3,33 +3,38 @@ from PIL import Image
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
+
 class MapDataset(Dataset):
     def __init__(self, root_dir):
         # saving path to folder with images
         self.root_dir = root_dir
 
         # creating list with names of all files in this dir
-        self.list_files = os.listdir(self.root_dir)
+        self.list_files = [
+            f for f in os.listdir(self.root_dir) if not f.startswith(".")
+        ]
 
         # definition of transforming every image
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-        ]) 
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
 
     def __len__(self):
         # returning number of the images
         return len(self.list_files)
-    
+
     def __getitem__(self, index):
         # getting file's name
         img_file = self.list_files[index]
 
         # getting full path
-        img_path = os.join(self.root_dir, img_file)
+        img_path = os.path.join(self.root_dir, img_file)
 
         # opening the image using PIL (RGB ensures us of having three colour channels)
-        image = Image.open(img_path).convert('RGB')
+        image = Image.open(img_path).convert("RGB")
 
         # data from dataset maps is horizontally concatenated so we need to split it excatly in a half
         # checking width and height of the image
