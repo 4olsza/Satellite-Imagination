6ff8e37ead6fa639f18ad4224cafae714be91604
kristoffer05 <krzyjak005@gmail.com>
2026-06-05 22:43:00 +0200

    traing.py: Working on train file

--- train.py
+++ train.py
@@ -7,3 +7,27 @@ from tqdm import tqdm # for showing progress bar in terminal
 from src.data.dataset import MapDataset
 from src.models.generator import Generator
 from src.models.discriminator import Discriminator
+
+# hiperparameters
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 16
+NUM_EPOCHS = 100
+
+def main():
+    print(f"Starting on device: {DEVICE}")
+
+    # initialazing models and sending them to the graphic card
+    disc = Discriminator(in_channels=3).to(DEVICE)
+    gen = Generator(in_channels=3, out_channels=3).to(DEVICE)
+
+    # preparing data (dataset and dataloader)
+    dataset = MapDataset(root_dir="data/maps/train")
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    print(f"Data prepared. There are {len(dataloader)} batches to work on")
+
+# security chech - allowing code to start only when calling the file
+if __name__ == "__main__":
+    main()
+
