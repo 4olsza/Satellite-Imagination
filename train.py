a06457dd9416974f07a3aa5dfe811bc279bd426e
kristoffer05 <krzyjak005@gmail.com>
2026-06-10 22:11:27 +0200

    train.py: Ending work with train file

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from src.data.dataset import MapDataset
 from src.models.generator import Generator
 from src.models.discriminator import Discriminator
 
-# hiperparameters
+# hyperparameters
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 16
@@ -17,9 +17,9 @@ NUM_EPOCHS = 100
 def main():
     print(f"Starting on device: {DEVICE}")
 
-    # initialazing models and sending them to the graphic card
-    disc = Discriminator(in_channels=3).to(DEVICE)
-    gen = Generator(in_channels=3, out_channels=3).to(DEVICE)
+    # initializing models and sending them to the GPU
+    discriminator = Discriminator(in_channels=3).to(DEVICE)
+    generator = Generator(in_channels=3, out_channels=3).to(DEVICE)
 
     # preparing data (dataset and dataloader)
     dataset = MapDataset(root_dir="data/maps/train")
@@ -27,7 +27,70 @@ def main():
 
     print(f"Data prepared. There are {len(dataloader)} batches to work on")
 
-# security chech - allowing code to start only when calling the file
+    # Loss Functions
+    # BCE judges how much model made a mistake in true / false grade
+    bce_loss = nn.BCEWithLogitsLoss()
+    # using WithLogits to change numbers to 0-1 before counting the error
+
+    # L1 Loss (Mean Absolute Error): checks pixel after pixel how far is generated image from real one
+    l1_loss = nn.L1Loss()
+
+    #L1 weight
+    L1_LAMBDA = 100
+
+    # Optimizers
+    opt_discriminator = optim.Adam(discriminator.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+    opt_generator = optim.Adam(generator.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+    # parameter betas=(0.5, 0.999) makes chaotic training of GANs more stable
+
+    # Training Loop
+    for epoch in range(NUM_EPOCHS):
+        print(f"\n--- EPOCH {epoch+1}/{NUM_EPOCHS} ---")
+
+        # putting dataloader in progress bar
+        loop = tqdm(dataloader, leave=True)
+
+        for idx, (x, y) in enumerate(loop):
+            # x: sketch (condition), y: real image from satellite (goal)
+            x, y = x.to(DEVICE), y.to(DEVICE)
+
+            y_fake = generator(x)
+
+            # STEP A: training discriminator - goal: teach discriminator to give 1 for originals and 0 for generated (false)
+            # looking at real pair at first
+            D_origin = discriminator(x, y)
+            loss_D_origin = bce_loss(D_origin, torch.ones_like(D_origin)) # we want to have result 1 (True) -> ones_like
+
+            # looking at fake pair (sketch + generated image)
+            D_generated = discriminator(x, y_fake.detach()) # using detach for training only discriminator
+            loss_D_generated = bce_loss(D_generated, torch.zeros_like(D_generated)) # we want to have result 0 (False) -> zeros_like
+
+            loss_D = (loss_D_origin + loss_D_generated) / 2
+
+            # clearing gradients
+            discriminator.zero_grad() # removing old memory
+            loss_D.backward() # backpropagation
+            opt_discriminator.step()
+
+            # STEP B: training generator - goal: forcing 1 on discriminator and learn to colour generated image
+            D_generated2 = discriminator(x, y_fake) # without detach because we are training generator
+            loss_G_generated = bce_loss(D_generated2, torch.ones_like(D_generated2)) # we want to force 1 -> ones_like
+            loss_G_L1 = l1_loss(y_fake, y) * L1_LAMBDA # checking differences in colours between generated and original
+
+            # total loss of generator
+            loss_G = loss_G_generated + loss_G_L1
+
+            # clearing gradients
+            generator.zero_grad()
+            loss_G.backward()
+            opt_generator.step()
+
+            # progress bar update
+            loop.set_postfix(
+                D_loss = loss_D.item(),
+                G_loss = loss_G.item(),
+            )
+
+# security check - allowing code to start only when calling the file
 if __name__ == "__main__":
     main()
-
